4764b81d860ec279064bdb246e3a6562f2f6bcce
eduardordl97 <erdl5259@gmail.com>
2021-08-01 20:39:21 -0500

    Creando Repositorios

---
+++ users-app/src/main/java/com/devs4j/users/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.devs4j.users.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devs4j.users.entities.Role;
+
+@Repository
+public interface RoleRepository  extends CrudRepository<Role, Integer> {
+
+}
